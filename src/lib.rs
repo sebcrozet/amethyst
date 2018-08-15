effe28733804043cb7c68e3df5e6cba30fdb62d8
Ellie <hi@ellie.moe>
2018-08-16 00:40:26 +0200

    Start implementing XR

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,7 @@ pub extern crate amethyst_locale as locale;
 pub extern crate amethyst_renderer as renderer;
 pub extern crate amethyst_ui as ui;
 pub extern crate amethyst_utils as utils;
+pub extern crate amethyst_xr as xr;
 pub extern crate winit;
 
 extern crate amethyst_ui;
